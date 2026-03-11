1e33bb7ba0c77a1b906cc7ad8ba51f6f22134531
aksil03 <aksil.perso@gmail.com>
2026-03-11 16:44:56 +0100

    base bdd sqlite

---
+++ bdd/bdd.js
@@ -0,0 +1,22 @@
+import sqlite3 from 'sqlite3';
+const db = new sqlite3.Database('./bdd/database.sqlite');
+
+db.serialize(() => {
+  db.run(`
+    CREATE TABLE IF NOT EXISTS utilisateurs (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      nom TEXT,
+      prenom TEXT,
+      email TEXT UNIQUE,
+      password TEXT
+    )
+  `, (err) => {
+    if (err) {
+      console.error(err.message);
+    } else {
+      console.log("table crée");
+    }
+  });
+});
+
+db.close();
