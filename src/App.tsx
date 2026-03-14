e446857f3bfa3306b9c28956d848482df020de49
aksil03 <aksil.perso@gmail.com>
2026-03-14 15:19:40 +0100

    feat: initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,26 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Navbar } from "./components/Navbar";
 import Acc from "./pages/Accueil";
-import Conn from "./pages/Connexion";
+import Conn from "./pages/Connexion/Connexion";
 import Dash from "./pages/Dashboard";
+import Insc from "./pages/Connexion/Inscription";
+
+export const CHEMIN_ACCUEIL = "/";
+export const CHEMIN_LOGIN = "/login";
+export const CHEMIN_DASHBOARD = "/dashboard";
+export const CHEMIN_INSCRIPTION = "/Inscription";
 
 function App() {
   return (
     <Router>
-      <div className="min-h-screen w-full  [background:radial-gradient(125%_125%_at_50%_10%,#fff_20%,#10b981_100%)] bg-fixed">
+      <div className="min-h-screen w-full [background:radial-gradient(125%_125%_at_50%_10%,#fff_20%,#10b981_100%)] bg-fixed">
         <Navbar />
         
         <Routes>
-          <Route path="/" element={<Acc />} />
-          
-          <Route path="/login" element={<Conn />} />
-          <Route path="/dashboard" element={<Dash />} />
+          <Route path={CHEMIN_ACCUEIL} element={<Acc />} />
+          <Route path={CHEMIN_LOGIN} element={<Conn />} />
+          <Route path={CHEMIN_DASHBOARD} element={<Dash />} />
+          <Route path={CHEMIN_INSCRIPTION} element={<Insc />} />
         </Routes>
       </div>
     </Router>
--- src/lib/queries.ts
+++ src/lib/queries.ts
@@ -1,12 +1,14 @@
 import { db } from './db.ts';
 import type { InscriptionData } from './types.ts'; 
 
+// recupere l'utilisateur via le mail
 export const getMail = async (email: string) => {
   return await db.utilisateur.findUnique({
     where: { email }
   });
 };
 
+// creer un user dans la bdd
 export const ajouterUtilisateur = async (userData: InscriptionData) => {
   return await db.utilisateur.create({
     data: userData
