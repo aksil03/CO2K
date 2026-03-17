d18134f2034a22cd1b173424e1e614e5ce05b55a
aksil03 <aksil.perso@gmail.com>
2026-03-17 10:49:35 +0100

    maj bdd et formulaire connexion

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -5,12 +5,16 @@ import { getMail } from "./queries";
 // type de recuperation d'attributs associée a un utilisateur via sont mail
 export type UserWithRelations = Awaited<ReturnType<typeof getMail>>;
 
+
 // schema d'inscription pour respecter la structure d'utilisateur voulu avant insertion en bdd
 export const InscriptionFormSchema = z.object({
   nom: z.string().min(1, "Le nom doit comporter au moins une lettre"),
   prenom: z.string().min(1, "Le prenom doit comporter au moins une lettre"),
   email: z.string().email("l'email doit etre au format standard"),
   password: z.string().min(4, "Le mot de passe doit comporter au moins 4 lettres"),
+  age: z.preprocess((val) => Number(val), z.number().int("L'âge doit être un nombre entier").min(15, "Vous devez avoir au minimum 15 ans")),
+  taille: z.preprocess((val) => Number(val), z.number().int("La taille doit être un nombre entier").min(50, "La taille minimum requise est de 50 cm")),
+  poids: z.preprocess((val) => Number(val), z.number().min(20, "Le poids minimum requis est de 20 kilos")),
 });
 
 
