fe149d44ac361da9ae7dce574aa270ce1e5b8453
aksil03 <aksil.perso@gmail.com>
2026-05-19 20:14:13 +0200

    test

--- server.ts
+++ server.ts
@@ -23,9 +23,9 @@ app.use(planningRoutes);
 app.use(programmesRoutes);
 app.use(communauteRoutes);
 
-app.listen(3000, () => {
-  console.log("Serveur démarré sur le port 3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Serveur démarré sur le port ${PORT}`);
 });
-
 
 
